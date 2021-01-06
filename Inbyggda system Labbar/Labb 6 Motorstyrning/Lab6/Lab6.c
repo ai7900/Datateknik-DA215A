e6a68438eaac752762a58f2c8b38f3ecc297e6a0
Johan Fritiofsson <47355404+master-johan@users.noreply.github.com>
2021-01-06 12:58:07 +0100

    small update

--- Inbyggda system Labbar/Labb 6 Motorstyrning/Lab6/Lab6.c
+++ Inbyggda system Labbar/Labb 6 Motorstyrning/Lab6/Lab6.c
@@ -6,11 +6,13 @@
  */ 
 
 #include <xc.h>
+#include <stdio.h>
 #include "delay/delay.h"
 #include "hmi/hmi.h"
 #include "lcd/lcd.h"
 #include "numkey/numkey.h"
 #include "regulator/regulator.h"
+#include "common.h"
 
 enum state
 {
@@ -33,18 +35,34 @@ int main(void)
 	
     while(1)
     {
+		
+		key = numkey_read();
         switch(currentState)
 		{
 			case MOTOR_ON:
+			if (regulator_read_power() > 0)
+			{
+				nextState = MOTOR_RUNNING;
+			}
+			else if (key == '1')
+			{
+				nextState = MOTOR_OFF;
+			}
+			output_msg("MOTOR ON","INSER POWER HERE", 0); // TODO:
 			break;
 			
 			case MOTOR_OFF:
-				key = numkey_read();
 				if(key == '2' && regulator_read_power() == 0)
 				nextState = MOTOR_ON;
+				output_msg("MOTOR OFF","",0);
 			break;
 			
 			case MOTOR_RUNNING:
+			
+			if (key == '1')
+			nextState= MOTOR_OFF;
+			
+			output_msg("MOTOR RUNNING", "INSERT POWER HERE",0); // TODO:
 			break;
 		}
 		
