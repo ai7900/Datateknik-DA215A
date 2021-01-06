7b64d3a284d56053c12c6ff38c7907d75e784549
Mattias Ståhlberg <46970089+ai7900@users.noreply.github.com>
2021-01-06 13:31:56 +0100

    added motor .c och .h

---
+++ Inbyggda system Labbar/Labb 6 Motorstyrning/Lab6/motor/motor.c
@@ -0,0 +1,6 @@
+/*
+ * motor.c
+ *
+ * Created: 2021-01-06 13:28:40
+ *  Author: matst
+ */ 
---
+++ Inbyggda system Labbar/Labb 6 Motorstyrning/Lab6/motor/motor.h
@@ -0,0 +1,15 @@
+/*
+ * motor.h
+ *
+ * Created: 2021-01-06 13:28:57
+ *  Author: matst
+ */ 
+
+
+#ifndef MOTOR_H_
+#define MOTOR_H_
+
+void motor_init(void);
+void motor_set_speed(uint8_t speed);
+
+#endif /* MOTOR_H_ */
