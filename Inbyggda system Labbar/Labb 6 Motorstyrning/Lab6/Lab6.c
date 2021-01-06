22d5703baa28be0f0fa4fccceca848af594e5d2e
Mattias Ståhlberg <46970089+ai7900@users.noreply.github.com>
2021-01-06 12:46:20 +0100

    FSM started

--- Inbyggda system Labbar/Labb 6 Motorstyrning/Lab6/Lab6.c
+++ Inbyggda system Labbar/Labb 6 Motorstyrning/Lab6/Lab6.c
@@ -2,7 +2,7 @@
  * main.c
  *
  * Created: 1/6/2021 11:38:59 AM
- *  Author: Mattias Ståhlberg, Johan Fristsson
+ *  Author: Mattias Ståhlberg, Johan Fritiofsson
  */ 
 
 #include <xc.h>
@@ -12,11 +12,49 @@
 #include "numkey/numkey.h"
 #include "regulator/regulator.h"
 
+enum state
+{
+	MOTOR_ON,
+	MOTOR_OFF,
+	MOTOR_RUNNING,
+};
+
+typedef enum state state_t;
+
+state_t currentState = MOTOR_OFF;
+state_t nextState = MOTOR_OFF;
+
+char key;
+
 
 int main(void)
 {
+	init();
+	
     while(1)
     {
-        //TODO:: Please write your application code 
+        switch(currentState)
+		{
+			case MOTOR_ON:
+			break;
+			
+			case MOTOR_OFF:
+				key = numkey_read();
+				if(key == '2' && regulator_read_power() == 0)
+				nextState = MOTOR_ON;
+			break;
+			
+			case MOTOR_RUNNING:
+			break;
+		}
+		
+		currentState = nextState;
     }
+}
+
+void init(void)
+{
+	hmi_init();
+	numkey_init();
+	regulator_init();
 }
