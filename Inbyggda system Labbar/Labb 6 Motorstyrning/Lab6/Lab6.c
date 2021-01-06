87f62485a183c2910086d974ead2150f325bcfc0
Mattias Ståhlberg <46970089+ai7900@users.noreply.github.com>
2021-01-06 11:46:13 +0100

    created lab6

---
+++ Inbyggda system Labbar/Labb 6 Motorstyrning/Lab6/Lab6.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 1/6/2021 11:38:59 AM
+ *  Author: Mattias Ståhlberg, Johan Fristsson
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
